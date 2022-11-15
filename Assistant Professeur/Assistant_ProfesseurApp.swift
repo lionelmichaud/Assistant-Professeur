29e9f8325492516a0efe5a3b12a217a104287b73
Lionel <lio.michaud@gmail.com>
2022-11-15 23:37:52 +0100

    Initial Commit

---
+++ Assistant Professeur/Assistant_ProfesseurApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assistant_ProfesseurApp.swift
+//  Assistant Professeur
+//
+//  Created by Lionel MICHAUD on 15/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Assistant_ProfesseurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
