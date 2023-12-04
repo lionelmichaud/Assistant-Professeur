c67aa548d8c967d6ef81ac37a6aa1d6a650222c9
Lionel <lio.michaud@gmail.com>
2023-12-04 19:28:26 +0100

    Fix Bug #3 - Improvement avec boucle d'attente

--- Assistant Professeur/Views/HomeScreen.swift
+++ Assistant Professeur/Views/HomeScreen.swift
@@ -27,6 +27,12 @@ struct HomeScreen: View {
     @Environment(UserContext.self)
     private var userContext
 
+    @State
+    private var userContextIsValid = false
+
+    @State
+    private var timeOut = false
+
     var body: some View {
         let userIsAuthenticatedOrAuthorized =
             authentication.userIsAuthenticatedByApple ||
@@ -37,29 +43,55 @@ struct HomeScreen: View {
                 // User pas encore authentifié ou autorisé
                 SignInView(showAlert: false)
 
-            } else if userContext.isValid {
+            } else if userContextIsValid {
                 // User authentifié ou autorisé ET
                 // User context valide
                 ContentView()
-                #if os(macOS)
-                    .frame(minWidth: 800, minHeight: 600)
-                #endif
+
+            } else if !timeOut {
+                // User authentifié ou autorisé ET
+                // User context NON valide
+                // Timeout de synchronisation NON échu
+                ProgressView(label: { Text("Synchronisation iCloud en cours...\nCela peut prendre plusieurs minutes.") })
+                    .font(.title2)
 
             } else {
                 // User authentifié ou autorisé ET
                 // User context NON valide
-                SignInView(showAlert: true)
-                #if os(macOS)
-                .frame(minWidth: 800, minHeight: 600)
-                #endif
+                // Timeout de synchronisation échu
+                Text("Erreur de synchronisation.\nEssayer plus tard.")
+                    .font(.title2)
+                    .foregroundStyle(.red)
             }
         }
+        #if os(macOS)
+        .frame(minWidth: 800, minHeight: 600)
+        #endif
+
         .task {
             // Vérifier si l'utilisateur est déjà autorisé.
             // Si oui, mettre à jour les context utilisateur.
             await authentication.checkUserAppleIdCredentials(
                 userContext: userContext
             )
+            userContextIsValid = userContext.isValid
+
+            // Attendre que iCloud ait synchronisé les données utilisateur
+            let period = 15 // seconds
+            var counter = 0
+            while !userContext.isValid {
+                try? await Task.sleep(for: .seconds(period))
+                if let userIdentifier = authentication.userCredentials?.userIdentifier,
+                   let owner = OwnerEntity.byUserIdentifier(userIdentifier: userIdentifier) {
+                    userContext.setOwner(to: owner)
+                }
+                counter += period
+                if counter > 5 * 60 {
+                    timeOut = true
+                    break
+                }
+            }
+            userContextIsValid = userContext.isValid
         }
     }
 }
